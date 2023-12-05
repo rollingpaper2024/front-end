7a81d018d1264b69efee6a504eb348b0a5ac756c
leesuyoen <dudgk0216@sqkcloud.com>
2023-12-05 16:36:33 +0900

    dosc 라우 설정 변경

--- src/router/routerData.tsx
+++ src/router/routerData.tsx
@@ -15,41 +15,49 @@ export const routerData: RouterElement[] = [
       id: 0,
       path: '/',
       element: <LandingPage/>,
+      withAuth:false
     },
     {
       id: 1,
       path: '/main',
       element:<Main/>,
+      withAuth:false
     },
     {
       id: 2,
       path: '/messagelist',
       element:<MessageList/>,
+      withAuth:false
     },
     {
       id: 3,
       path: '/selectpocket',
       element:<SelectPocket/>,
+      withAuth:true
     },
     {
       id: 4,
       path: '/sendlandingpage',
       element:<SendLandingPage/>,
+      withAuth:true
     },
     {
       id: 5,
       path: '/sharepage',
       element:<SharePage/>,
+      withAuth:true
     },
     {
       id: 6,
       path: '/writemessage',
       element:<WriteMessage/>,
+      withAuth:true
     },
     {
       id: 6,
       path: '/login',
       element:<LoginPage/>,
+      withAuth:false
     },
   ]
   
