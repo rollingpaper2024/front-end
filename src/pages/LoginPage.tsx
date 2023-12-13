e35081aed1e0a8c7e6ac9c8285ff1adb5c92828d
양효빈 <yangnorong@gmail.co>
2023-12-13 09:58:47 +0900

    feat: kakaologin 세팅중

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import { onClickSocialLogin } from '@/api'
 
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+  
+const Login = () => {
+  if (!window.Kakao.isInitialized()) {
+    window.Kakao.init(import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY);
+  }
+
+  return (...);
+}
+
+
+const onLoginWithKakao = () => {
+  const redirectUri = `${location.origin}/callback/kakaotalk`;
+  const scope = [
+    KAKAO_SCOPE_NICKNAME,
+
+  ].join(",");
+
+  window.Kakao.Auth.authorize({
+    redirectUri,
+    scope,
+  });
+};
+
 function LoginPage() {
   return (
     <div><button name="google" onClick={onClickSocialLogin}>google</button></div>
+    <button onClick={onLoginWithKakao}>카카오 로그인</button>
   )
 }
 
 export default LoginPage
+
