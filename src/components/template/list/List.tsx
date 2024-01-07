871000d0b18abc4fea254e2c91a04271ad1072ba
[IoT1]이수연 <dudgk0216@gmail.com>
2024-01-07 21:26:40 +0900

    fire base api 로직 수정

--- src/components/template/list/List.tsx
+++ src/components/template/list/List.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react"
 import * as Styled from "./list.styled"
 import MainTitle from '@/components/molecule/title/MainTitle'
 
-function List() {
-  const [messageCount, setMessageCount] = useState(0)
-  
-  // 파이어 베이스에서 유저의 덕담 개수를 가져와서 setMessageCount 한다
+function List({messageCount}:{messageCount:number}) {
+
+
   return (
     <Styled.SLayout>
          <MainTitle title={`${messageCount}개의 덕담 도착!`} desc='마음을 담아 보내진 덕담들을 읽어보세요'/>
--- src/pages/WriteMessage.tsx
+++ src/pages/WriteMessage.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import {postData}  from '@/api'
-import { v4 as uuidv4 } from 'uuid';
+import { useEffect, useState } from 'react';
+import { postData } from '@/api'
+import { onAuthStateChanged,getAuth,User } from "firebase/auth";
+import { app } from "@/database";
 
 function WriteMessage() {
+     const auth = getAuth(app);
+  const [userId, setUserId] = useState('');
+
+    useEffect(() => {
+    // 현재 로그인한 사용자 확인
+    const unsubscribe = onAuthStateChanged(auth, (user: User )  => {
+      if (user) {
+        setUserId(user.uid);
+      } else {
+        setUserId('');
+      }
+    });
+
+    return () => unsubscribe(); 
+    }, []);
+  
+  console.log("test",userId)
+  
   function test() {
-    postData('Message', { writer: '슈짱', contents:'새해복 많이 받아',uid:uuidv4(),pocket:1})
+    postData('Message', { writer: '슈슈', contents:'갑진년 새해복 많이 받아',uid:userId,pocket:1})
     // post 요청할때 이렇게 공통 함수 import 해서 하면 됩니다. pocket은 pocket의 색상 번호 입니다.
     
   }
