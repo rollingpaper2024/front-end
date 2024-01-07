ebcfcf914d803f1448e72ae260c151aee3e3af36
[IoT1]이수연 <dudgk0216@gmail.com>
2024-01-07 21:37:54 +0900

    MainTitle 수정

--- src/components/template/list/List.tsx
+++ src/components/template/list/List.tsx
@@ -6,7 +6,7 @@ function List({messageCount}:{messageCount:number}) {
 
   return (
     <Styled.SLayout>
-         <MainTitle title={`${messageCount}개의 덕담 도착!`} desc='마음을 담아 보내진 덕담들을 읽어보세요'/>
+     <MainTitle title={`${messageCount}개의 덕담 도착!`} desc='마음을 담아 보내진 덕담들을 읽어보세요'/>
     </Styled.SLayout>
   )
 }
