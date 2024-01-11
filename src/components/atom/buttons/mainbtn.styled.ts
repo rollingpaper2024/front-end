20b83e1af948c0a8113f2e85b53f2604ccb57f90
양효빈 <yangnorong@gmail.co>
2024-01-11 11:19:40 +0900

    style: main버튼 패턴 수정

--- src/components/atom/buttons/mainbtn.styled.ts
+++ src/components/atom/buttons/mainbtn.styled.ts
@@ -1,16 +1,17 @@
 import Color from '@/style/Color'
 import styled from 'styled-components'
-import * as Font from '@/style/Font'
+import * as font from '@/style/font'
+import { Device } from '@/style/Device'
 
 export const SBtn = styled.button<{ $isDisabled: boolean }>`
   background-color: ${(props) => (props.$isDisabled ? Color.gray[500] : Color.gray[600])};
   height: 56px;
   width: 80%;
   color: ${Color.yellow[300]};
-  font-size: ${Font.Fontsize.Small};
-  font-weight: ${Font.Fontweight.Bold};
+  font-size: ${font.Fontsize.Small};
+  font-weight: ${font.Fontweight.Bold};
   border: 2px solid ${Color.gray[600]};
-  min-width: 334px;
+  min-width: 180px;
   overflow: hidden;
 
   // 버튼이 비활성화 상태가 아닐 때 hover 및 active 스타일 적용
@@ -40,19 +41,18 @@ export const SBtn = styled.button<{ $isDisabled: boolean }>`
 `
 export const LeftBtn = styled.div`
   position: relative;
-  top: -2px;
 `
 
 export const RightBtn = styled.div`
   transform: rotate(180deg);
-  top: -76px;
+  top: -70px;
   position: relative;
 `
 
 export const BtnText = styled.div`
   text-align: center;
   position: relative;
-  top: -55%;
+  top: -50%;
   font-weight: 600;
   left: 50%;
   transform: translate(-50%, -50%);
