ba964af84eaab06954ca936d1102cb695f79d1d3
양효빈 <yangnorong@gmail.co>
2024-01-12 11:14:27 +0900

    fixed: mainBtn font 위치 css 수정

--- src/components/atom/buttons/mainbtn.styled.ts
+++ src/components/atom/buttons/mainbtn.styled.ts
@@ -58,6 +58,9 @@ export const BtnText = styled.div`
   transform: translate(-50%, -50%);
 
   @media ${Devices.tablet} {
-    position: relative;
+    position: absolute;
+    top: -50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 `
