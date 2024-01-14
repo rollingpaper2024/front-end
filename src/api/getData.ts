fc6901584f066232a580a49b61309c920ec51687
[IoT1]이수연 <dudgk0216@gmail.com>
2024-01-14 23:50:18 +0900

    react query 이용한 무한스크롤 구현

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -1,4 +1,12 @@
-import { collection, query, where, getDocs, limit, QueryDocumentSnapshot } from 'firebase/firestore'
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  limit,
+  QueryDocumentSnapshot,
+  startAfter,
+} from 'firebase/firestore'
 import { db } from '@/database'
 
 interface messageType {
@@ -8,26 +16,52 @@ interface messageType {
   createdAt: string
   pocket: string
 }
-// lastVisible은 마지막 데이터일때
-// 특정 사용자의 방명록만 가져오는 함수
-async function getUserMessages(collectionName: string, userId: string, pageSize?: number) {
+
+async function getUserMessages(
+  collectionName: string,
+  userId: string,
+  lastDoc?: null,
+  pageSize?: number,
+) {
   try {
-    let q = query(collection(db, collectionName), where('uid', '==', userId))
+    let q
+
     if (pageSize) {
-      q = query(q, limit(pageSize))
+      if (lastDoc) {
+        q = query(
+          collection(db, collectionName),
+          where('uid', '==', userId),
+          startAfter(lastDoc),
+          limit(pageSize),
+        )
+      } else {
+        q = query(collection(db, collectionName), where('uid', '==', userId), limit(pageSize))
+      }
+      const querySnapshot = await getDocs(q)
+      const messages: messageType[] = []
+
+      querySnapshot.forEach((doc) => {
+        const messageData = doc.data() as messageType
+        messages.push(messageData)
+      })
+
+      return { messages, lastVisible: querySnapshot.docs[querySnapshot.docs.length - 1] }
+    } else {
+      // pageSize가 제공되지 않은 경우, 모든 항목 반환
+      q = query(collection(db, collectionName), where('uid', '==', userId))
+      const querySnapshot = await getDocs(q)
+      const messages: messageType[] = []
+
+      querySnapshot.forEach((doc: QueryDocumentSnapshot) => {
+        const messageData = doc.data() as messageType
+        messages.push(messageData)
+      })
+
+      return messages
     }
-
-    const querySnapshot = await getDocs(q)
-    const messages: messageType[] = []
-
-    querySnapshot.forEach((doc: QueryDocumentSnapshot) => {
-      const messageData = doc.data() as messageType // 타입 단언 사용
-      messages.push(messageData)
-    })
-
-    return messages
   } catch (err) {
     console.error('Error getting documents: ', err)
+    return []
   }
 }
 
--- src/pages/MessageList.tsx
+++ src/pages/MessageList.tsx
@@ -1,102 +1,69 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useEffect, useState, useRef } from 'react'
-import { getUserMessages } from '@/api'
-import List from '@/components/template/list/List'
-import { useAtom } from 'jotai'
-import { userAtom } from '@/store/user'
-import { useRouter } from '@/hooks/useRouter'
+import React, { useRef, useEffect, useState } from 'react'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import ListCard from '@/components/atom/card/ListCard'
 import uuid from 'react-uuid'
 import * as Styled from '@/components/organism/list/Listlayer.styled'
 import { useParams } from 'react-router-dom'
+import { useAtom } from 'jotai'
+import { userAtom } from '@/store/user'
+import { getUserMessages } from '@/api'
+import List from '@/components/template/list/List'
 
 function MessageList() {
   const [user] = useAtom(userAtom)
-  const [messages, setMessages] = useState<any[]>([])
-  const [messageCount, setMessageCount] = useState(0)
-  const { routeTo } = useRouter()
-  const [loading, setLoading] = useState(false)
   const containerRef = useRef<HTMLDivElement>(null)
-  const [listindex, setListIndex] = useState(6)
   const { id } = useParams()
+  const [messageCount, setMessageCount] = useState(0)
 
   useEffect(() => {
+    // 전체 메시지 개수를 가져오는 함수
+    const fetchMessageCount = async () => {
+      const totalMessageData = await getUserMessages('Message', id)
+      if (totalMessageData) {
+        setMessageCount(totalMessageData?.length)
+      }
+    }
+
     if (user) {
-      fetchMessages()
+      fetchMessageCount()
     }
-  }, [user])
+  }, [user, id])
 
-  //무한스크롤
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollHeight, scrollTop, clientHeight } = containerRef.current || {}
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ['messages', id],
+    queryFn: ({ pageParam = null }) => getUserMessages('Message', id, pageParam, 6),
+    getNextPageParam: (lastPage) => lastPage.lastVisible || undefined,
+  })
 
-      if (
-        scrollTop + clientHeight >= scrollHeight - 1 &&
-        !loading &&
-        messages.length < messageCount
-      ) {
-        setListIndex((listIndex) => listIndex + 6)
-      }
-    }
-
+  const handleScroll = () => {
     if (containerRef.current) {
-      containerRef.current.addEventListener('scroll', handleScroll)
-    }
-
-    return () => {
-      if (containerRef.current) {
-        containerRef.current.removeEventListener('scroll', handleScroll)
+      const { scrollHeight, scrollTop, clientHeight } = containerRef.current
+      if (scrollTop + clientHeight >= scrollHeight - 1 && hasNextPage && !isFetchingNextPage) {
+        fetchNextPage()
       }
-    }
-  }, [containerRef, loading, messages, messageCount])
-
-  useEffect(() => {
-    fetchMessages()
-  }, [listindex])
-
-  async function fetchMessages() {
-    try {
-      setLoading(true)
-      const data = await getUserMessages('Message', id, listindex)
-      const allData = await getUserMessages('Message', id)
-
-      if (data && allData) {
-        // setMessages((prevMessages) => [...prevMessages, ...data])
-        setMessages(data)
-        setMessageCount(allData.length)
-        //console.log(data)
-      }
-    } catch (error) {
-      console.error(error)
-    } finally {
-      setLoading(false)
     }
   }
 
   return (
     <>
       <List messageCount={messageCount} />
-      <Styled.SLayout ref={containerRef}>
-        {messages.map((message) => (
-          <ListCard
-            key={uuid()}
-            color="#FFC44F"
-            name={message.writer}
-            date={message.createdAt}
-            message={message.contents}
-          />
+      <Styled.SLayout ref={containerRef} onScroll={handleScroll}>
+        {data?.pages.map((page, i) => (
+          <React.Fragment key={i}>
+            {page.messages.map((message) => (
+              <ListCard
+                key={uuid()}
+                color="#FFC44F"
+                name={message.writer}
+                date={message.createdAt}
+                message={message.contents}
+              />
+            ))}
+          </React.Fragment>
         ))}
-        {loading && <p>Loading...</p>}
-        {!loading && messages.length === messageCount && <p>모든 덕담리스트를 확인했습니다.</p>}
+        {isFetchingNextPage && <p>Loading more...</p>}
+        {!hasNextPage && <p>모든 덕담리스트를 확인했습니다.</p>}
       </Styled.SLayout>
-      {/*<BtnArea
-        onClick={() => {
-          routeTo(`/writemessage/${user.uid}`)
-        }}
-        title="덕담 쓰기"
-        isDisabled={false}
-      />*/}
     </>
   )
 }
