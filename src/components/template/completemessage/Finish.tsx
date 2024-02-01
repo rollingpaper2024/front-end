f515a44ba1de5c9908dedd50a6e17ea0e45e1b65
양효빈 <yangnorong@gmail.co>
2024-02-01 16:58:10 +0900

    feat:덕담 작성 완료 후 모달 생성

--- src/components/template/completemessage/Finish.tsx
+++ src/components/template/completemessage/Finish.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import MainTitle from '@/components/molecule/title/MainTitle'
 import lottie from 'lottie-web'
 import * as Styled from './finish.styled'
@@ -6,10 +6,13 @@ import BtnArea from '@/components/molecule/layout/BtnArea'
 import { useRouter } from '@/hooks/useRouter'
 import { useAtom } from 'jotai'
 import { userAtom } from '@/store/user'
+import FinishModal from '@/components/organism/modal/FinishModal'
 
 function Finish() {
   const likecontainer = useRef(null)
   const [user] = useAtom(userAtom)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isFirstClick, setIsFirstClick] = useState(true) // 처음 클릭 여부를 관리합니다.
 
   useEffect(() => {
     if (likecontainer.current) {
@@ -25,19 +28,30 @@ function Finish() {
 
   const { routeTo } = useRouter()
 
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleBtnClick = () => {
+    if (isFirstClick) {
+      openModal()
+      setIsFirstClick(false) // 처음 클릭 이후에는 다음 클릭에서 모달을 열지 않음
+    } else {
+      // 다음 클릭에서는 페이지 이동 수행
+      routeTo(`/main/${user.uid}`)
+    }
+  }
+
   return (
     <>
+      <FinishModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
       <MainTitle title="덕담 발송 완료!" desc="따뜻한 덕담이 발송되었어요" />
       <Styled.SWrapper ref={likecontainer}></Styled.SWrapper>
-      {
-        <BtnArea
-          onClick={() => {
-            routeTo(`/main/${user.uid}`)
-          }}
-          title="메인으로"
-          isDisabled={false}
-        />
-      }
+      <BtnArea
+        onClick={handleBtnClick} // 클릭 이벤트를 handleBtnClick 함수로 변경
+        title={isFirstClick ? '메인으로' : '다시 메인으로'} // 클릭 상태에 따라 버튼 텍스트 변경
+        isDisabled={false}
+      />
     </>
   )
 }
