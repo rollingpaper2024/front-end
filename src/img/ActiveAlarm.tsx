8f35d3523b8043afff0d527e136193c72d66673d
양효빈 <yangnorong@gmail.co>
2024-02-05 16:36:21 +0900

    style: header icon size 수정

--- src/img/ActiveAlarm.tsx
+++ src/img/ActiveAlarm.tsx
@@ -1,26 +1,14 @@
 const ActiveAlarm = () => {
   return (
-    <svg width="40" height="40" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M7 10C7 10.3333 6.9 11.5 6.5 13.5" stroke="#2F2F31" stroke-width="2" />
-      <path d="M17 10C17 10.3333 17.1 11.5 17.5 13.5" stroke="#2F2F31" stroke-width="2" />
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
       <path
-        d="M15 18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18"
         stroke="#2F2F31"
         stroke-width="2"
+        d="M7 10c0 .333-.1 1.5-.5 3.5M17 10c0 .333.1 1.5.5 3.5M15 18a3 3 0 1 1-6 0m8-8a5 5 0 0 0-10 0"
       />
-      <path
-        d="M17 10C17 7.23858 14.7614 5 12 5C9.23858 5 7 7.23858 7 10"
-        stroke="#2F2F31"
-        stroke-width="2"
-      />
-      <path
-        d="M14 5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5"
-        stroke="#2F2F31"
-        stroke-width="2"
-      />
-      <path d="M5 18L19 18" stroke="#2F2F31" stroke-width="2" stroke-linecap="round" />
-      <path d="M7 10C6.83333 12 6 17.5 5 18" stroke="#2F2F31" stroke-width="2" />
-      <path d="M17 10C17.1667 12 18 17.5 19 18" stroke="#2F2F31" stroke-width="2" />
+      <path stroke="#2F2F31" stroke-width="2" d="M14 5a2 2 0 1 0-4 0" />
+      <path stroke="#2F2F31" stroke-linecap="round" stroke-width="2" d="M5 18h14" />
+      <path stroke="#2F2F31" stroke-width="2" d="M7 10c-.167 2-1 7.5-2 8m12-8c.167 2 1 7.5 2 8" />
       <circle cx="16.5" cy="5.5" r="3.5" fill="#EF961C" />
     </svg>
   )
