5c39c6f2da266c5c51bd37e13f213070a453cd16
[IoT1]이수연 <dudgk0216@gmail.com>
2024-02-09 00:25:37 +0900

    컬러칩 수정

--- src/img/BluePocket.tsx
+++ src/img/BluePocket.tsx
@@ -1,14 +1,12 @@
-
-
 const BluePocket = () => {
-    return (
-       <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none">
-            <circle cx="32" cy="32" r="32" fill="#A6ADFB" />
-             <text x="50%" y="52%" dominantBaseline="middle" textAnchor="middle"fill="#FFFEFC">
-                자개
-            </text>
-        </svg>
-    )
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none">
+      <circle cx="32" cy="32" r="32" fill="#2A678B" />
+      <text x="50%" y="52%" dominantBaseline="middle" textAnchor="middle" fill="#FFFEFC">
+        파랑
+      </text>
+    </svg>
+  )
 }
 
 export default BluePocket
